8d6951a773c0d7c5aad3fd57b4f4a2d9cb4e257d
christian@voegtle.org <christian@voegtle.org>
2018-01-21 09:59:19 +0100

    Besseres Logging

--- src/main/kotlin/de/voegtle/wunschmanager/util/UserUtil.kt
+++ src/main/kotlin/de/voegtle/wunschmanager/util/UserUtil.kt
@@ -1,11 +1,13 @@
 package de.voegtle.wunschmanager.util
 
 import org.voegtle.wunschmanager.data.WishList
+import java.util.logging.Logger
 import javax.servlet.http.HttpServletRequest
 
 fun extractUserName(request: HttpServletRequest, exceptionIfNull: Boolean = true): String? {
   val userName = request.userPrincipal?.name
   if (exceptionIfNull && userName == null) {
+    logException("This action requires a login")
     throw LoginRequiredException("This action requires a login")
   }
   return userName
@@ -14,13 +16,20 @@ fun extractUserName(request: HttpServletRequest, exceptionIfNull: Boolean = true
 fun checkOwnership(request: HttpServletRequest, wishList: WishList, message: String) {
   val userName = extractUserName(request, true)
   if (wishList.owner != userName) {
+    logException("$userName: $message")
     throw PermissionDenied(message = message)
   }
 }
 
 fun checkNotOwnership(userName: String?, wishList: WishList, message: String) {
   if (wishList.owner == userName) {
+    logException("$userName: $message")
     throw PermissionDenied(message= message)
   }
+}
+
+private fun logException(message: String) {
+  val log = Logger.getLogger("UserUtil")
+  log.warning(message)
 }
 
--- src/main/kotlin/org/voegtle/wunschmanager/data/WishList.kt
+++ src/main/kotlin/org/voegtle/wunschmanager/data/WishList.kt
@@ -3,15 +3,14 @@ package org.voegtle.wunschmanager.data
 import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 import com.googlecode.objectify.annotation.Index
-import java.util.Date
 
 @Entity
 class WishList(@Id var id: Long? = null, @Index var owner: String? = null, @Index var event: String = "",
                var managed: Boolean = false, @Index var createTimestamp: Long? = null) : Comparable<WishList> {
   override fun compareTo(other: WishList): Int {
     return when {
-      createTimestamp?:0 < other.createTimestamp?:0 -> -1
-      createTimestamp?:0 > other.createTimestamp?:0 -> 1
+      createTimestamp ?: 0 < other.createTimestamp ?: 0 -> -1
+      createTimestamp ?: 0 > other.createTimestamp ?: 0 -> 1
       else -> 0
     }
   }
