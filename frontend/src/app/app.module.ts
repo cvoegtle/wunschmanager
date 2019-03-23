2034a52b2947a2627311d9b0bfd44bfdc15e7431
christian@voegtle.org <christian@voegtle.org>
2019-03-23 23:50:02 +0100

    Neue Version auf der AppEngine

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -63,7 +63,6 @@ import { FocusDirective } from "./util/focus.directive";
 
 import {A11yModule} from '@angular/cdk/a11y';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import {ScrollingModule} from '@angular/cdk/scrolling';
 import {CdkStepperModule} from '@angular/cdk/stepper';
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
