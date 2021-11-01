a1476e8bd511abd994f514ad41c903b06179c246
christian@voegtle.org <christian@voegtle.org>
2021-11-01 08:57:22 +0100

    upgrade auf Angular 12

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -57,5 +57,5 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';
+import 'zone.js';
 
