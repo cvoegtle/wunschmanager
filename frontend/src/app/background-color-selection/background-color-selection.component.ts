9d9ee2634fd704582361a5660d54fcd33ab2e840
christian@voegtle.org <christian@voegtle.org>
2025-08-02 18:25:57 +0200

    Color Picker Perfektionieren

--- frontend/src/app/background-color-selection/background-color-selection.component.ts
+++ frontend/src/app/background-color-selection/background-color-selection.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class BackgroundColorSelectionComponent {
   @Input() backgroundColor: string;
-  @Output() valueChange = new EventEmitter<String>();
+  @Output() valueChange = new EventEmitter<string>();
 
 
   onSelectionChange() {
